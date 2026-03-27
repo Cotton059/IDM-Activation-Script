756e06c954587640020f72d85da42fb7bb3772fe
Cotton059 <107194432+Cotton059@users.noreply.github.com>
2026-03-27 18:11:37 +0800

    Update download URL to use IAS_v2.cmd
    修复文件名错误

--- IAS_v2.ps1
+++ IAS_v2.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-$DownloadURL = 'https://raw.githubusercontent.com/Cotton059/IDM-Activation-Script/main/IAS.cmd'
+$DownloadURL = 'https://raw.githubusercontent.com/Cotton059/IDM-Activation-Script/main/IAS_v2.cmd'
 
 $rand = Get-Random -Maximum 99999999
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
