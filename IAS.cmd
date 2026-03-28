b5be66481c760f2325c631ffff41b4b8e7b8e828
Cotton059 <107194432+Cotton059@users.noreply.github.com>
2026-03-28 11:18:38 +0800

    Update IAS.cmd
    更新地址

--- IAS.cmd
+++ IAS.cmd
@@ -62,7 +62,7 @@ exit /b
 ::========================================================================================================================================
 
 set "blank="
-set "mas=https://github.com/hanmaoye/IDM-Activation-Script/wiki/"
+set "mas=https://github.com/Cotton059/IDM-Activation-Script/wiki/"
 
 ::  Check if Null service is working, it's important for the batch script
 
@@ -400,7 +400,7 @@ echo:
 call :_color2 %_White% "             " %_Green% "Create By Piash"
 echo:            ___________________________________________________ 
 echo:
-echo:               Github: https://github.com/hanmaoye
+echo:               Github: https://github.com/Cotton059
 echo:            ___________________________________________________ 
 echo:                                                               
 echo:               [1] Activate
@@ -418,7 +418,7 @@ choice /C:123450 /N
 set _erl=%errorlevel%
 
 if %_erl%==6 exit /b
-if %_erl%==5 start https://github.com/hanmaoye/IDM-Activation-Script& goto MainMenu
+if %_erl%==5 start https://github.com/Cotton059/IDM-Activation-Script& goto MainMenu
 if %_erl%==4 start https://www.internetdownloadmanager.com/download.html & goto MainMenu
 if %_erl%==3 goto _reset
 if %_erl%==2 (set frz=1&goto :_activate)
